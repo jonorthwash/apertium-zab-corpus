2f809b8d34a5982d78248a8936ea7af46863db3a
Jonathan Washington <jonathan.n.washington@gmail.com>
2021-03-07 21:11:13 -0500

    including Ticha post

--- coverage.sh
+++ coverage.sh
@@ -17,6 +17,7 @@ coverage-hfst FHL-poetry/FHL-poetry.all.txt ../apertium-zab/zab.automorf.hfst
 coverage-hfst Tlalocan/Tlalocan@Simp.all.txt ../apertium-zab/zab.automorf.hfst
 coverage-hfst Tlalocan/Tlalocan@Dict.all.txt ../apertium-zab/zab.automorf.hfst
 coverage-hfst Bible/SJGZ.bible.txt ../apertium-zab/zab.automorf.hfst
+coverage-hfst Ticha/2020-07-17.txt ../apertium-zab/zab.automorf.hfst
 
 Simp12line=$(grep 'BxTP@Simp.1-2.txt' history.log | tail -n1)
 Dict12line=$(grep 'BxTP@Dict.1-2.txt' history.log | tail -n1)
@@ -26,6 +27,7 @@ FHLline=$(grep 'FHL-poetry.all.txt' history.log | tail -n1)
 SimpTlalocan=$(grep 'Tlalocan@Simp.all.txt' history.log | tail -n1)
 DictTlalocan=$(grep 'Tlalocan@Dict.all.txt' history.log | tail -n1)
 bibleSJGZ=$(grep 'SJGZ.bible.txt' history.log | tail -n1)
+TichaPost=$(grep '2020-07-17.txt' history.log | tail -n1)
 
 Simp12cov=$(echo "$Simp12line" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
 Dict12cov=$(echo "$Dict12line" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
@@ -35,6 +37,7 @@ FHLcov=$(echo "$FHLline" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
 SimpTlcov=$(echo "$SimpTlalocan" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
 DictTlcov=$(echo "$DictTlalocan" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
 bibleSJGZcov=$(echo "$bibleSJGZ" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
+TichaPostcov=$(echo "$TichaPost" | cut -f3 | sed 's/~//' | sed 's/$/*100/' | bc )
 
 Simp12toks=$(echo "$Simp12line" | cut -f2 | sed 's/.*\///' )
 Dict12toks=$(echo "$Dict12line" | cut -f2 | sed 's/.*\///' )
@@ -44,6 +47,7 @@ FHLtoks=$(echo "$FHLline" | cut -f2 | sed 's/.*\///' )
 SimpTltoks=$(echo "$SimpTlalocan" | cut -f2 | sed 's/.*\///' )
 DictTltoks=$(echo "$DictTlalocan" | cut -f2 | sed 's/.*\///' )
 bibleSJGZtoks=$(echo "$bibleSJGZ" | cut -f2 | sed 's/.*\///' )
+TichaPosttoks=$(echo "$TichaPost" | cut -f2 | sed 's/.*\///' )
 
 
 #echo $(bc -l <<< "scale=2; $Dict12cov*100")
@@ -56,4 +60,5 @@ echo "        \emph{Blal xte Tiu Pamyël} 3-4 & Dictionary & $Dict36toks & $(pri
 echo "        Felipe H. Lopez poetry & Simple & $FHLtoks & $(printf %.2f $FHLcov) \\\\"
 echo "        Tlalocan poems \\& story & Simple & $SimpTltoks & $(printf %.2f $SimpTlcov) \\\\"
 echo "         & Dictionary & $DictTltoks & $(printf %.2f $DictTlcov) \\\\"
+echo "        Ticha post 2020-07-31 & Simple & $TichaPosttoks & $(printf %.2f $TichaPostcov) \\\\"
 echo "        SJGZ Bible & SJGZ & $bibleSJGZtoks & $(printf %.2f $bibleSJGZcov) \\\\"
